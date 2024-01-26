dd35476b2a209c2ef1e8214e3c936cc8733d7af6
Zhaoyu Yang <zoe.anhui@gmail.com>
2024-01-25 23:25:58 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
           ensure => present,
           user   => 'root',
           type   => 'ssh-rsa',
-          key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDexmCjfixgcWbr3FDAnXBUo90jvXgtuAkcsKYMPlk2JnrKjKTNAhQD9RvQxoSnhHyKUFPtZ3BO0oqArpaaeuBB1iqWJZRlJjMGMglkfK/ZGyNKPzDin8qxilW0I6zmxMW+38t0THCZpdhz62FakIYevhYPNMAGflga7N4nIbv9//BRe1SC6x55Zj8NH8QIBN94M18UtfFhuUYOyEPs2s+Eo+WSXtBx4IbX97J3pZOScSkOoPhjWzfS86oNwQO5Sgy1dzlLEp3MJRURfiM/ojtT6KJsD/8ygdAMJMqAEt9dzUjd/IShLRM1bzZ7AfqLzL/51T96Cma3Zpdh7thWYyU9',
+          key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDexmCjfixgcWbr3FDAnXBUo90jvXgtuAkcsKYMPlk2JnrKjKTNAhQD9RvQxoSnhHyKUFPtZ3BO0oqArpaaeuBB1iqWJZRlJjMGMglkfK/ZGyNKPzDin8qxilW0I6zmxMW+38t0THCZpdhz62FakIYevhYPNMAGflga7N4nIbv9//BRe1SC6x55Zj8NH8QIBN94M18UtfFhuUYOyEPs2s+Eo+WSXtBx4IbX97J3pZOScSkOoPhjWzfS86oNwQO5Sgy1dzlLEp3MJRURfiM/ojtT6KJsD/8ygdAMJMqAEt9dzUjd/IShLRM1bzZ7AfqLzL/51T96Cma3Zpdh7thWYyU9',
   }         
 }
