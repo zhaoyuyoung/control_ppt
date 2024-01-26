8f132c30e055e7f8318593e5ec4b9ccd5324b5e3
Zhaoyu Yang <zoe.anhui@gmail.com>
2024-01-26 00:07:26 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,9 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}\n",
   }
 }
-
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
 node /^web/ {
   include role::app_server
 }
