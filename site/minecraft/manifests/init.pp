718fe1619483b7fd3c95cd818da3ea36362c0952
Zhaoyu Yang <zoe.anhui@gmail.com>
2024-01-26 00:18:54 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -9,7 +9,7 @@ class minecraft {
   package {'java':
     ensure => present,
   }
-  file {'opt/minecraft/eula.txt':
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=ture',
   }
