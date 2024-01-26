8bb5cf8cdc34ea99d3d9ae74bd81ea458157bb27
Zhaoyu Yang <zoe.anhui@gmail.com>
2024-01-25 23:56:27 -0500

    Create init.pp

---
+++ site/mincraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=ture',
+  }
+  file {'/etc/systemd/system/mindcraft.service':
+    ensure => file
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
